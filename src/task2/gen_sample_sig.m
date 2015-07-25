c09fd862ac99b4825ff3d957a969cb178e58a17c
Thomas Lee <lisihan969@gmail.com>
2015-07-26 02:29:18 +0800

    Re-implement gen_sample_sig for multiple f

--- src/task2/gen_sample_sig.m
+++ src/task2/gen_sample_sig.m
@@ -1,5 +1,18 @@
 %% gen_sample_sig: Generate sample signal.
 function sig = gen_sample_sig(f, f_sample, duration)
-    period = floor(f_sample / f);
-    t = [1 : f_sample * duration]';
-    sig = double(mod(t, period) == 0);
+    sig = [];
+    offset = 0;
+    for k = 1:length(f)   % For every part.
+        period = floor(f_sample / f(k));
+        len = floor(f_sample * duration(k));
+        if len <= offset  % Skip this part.
+            sig = [sig; zeros(len, 1)];
+            offset = offset - len;
+        else
+            len = len - offset;
+            % Skip first `offset` elements.
+            t = [zeros(1, offset), 1:len]';
+            sig = [sig; double(mod(t, period) == 1)];
+            offset = mod(period - mod(len, period), period);
+        end
+    end
